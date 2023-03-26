da783d2bff3526cb6e8440fde42971b37f5e4b7f
alvigabriel <claudio.alves20112011@hotmail.com>
2023-03-26 10:13:57 -0300

    Add função que verifica agencia e conta

--- Models/Operacoes.cs
+++ Models/Operacoes.cs
@@ -3,24 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Banco.Models
 {
+
     public class Operacoes
     {
-         public void Op()
+        public string? Ag{ 
+            set
+            {
+                if(value != "123456")
+                {
+                    Console.WriteLine("Agência não encontrada");
+                }
+            }
+            }
+        public string? Conta{ 
+            set
+            {
+                if(value != "1234")
+                {
+                    Console.WriteLine("Conta não encontrada");
+                }
+            }
+        }
+        public int Saldo{ get; set; }
+
+        public void Login()
         {
             Console.WriteLine("Seja bem vindo!");
+
+            Console.WriteLine("Digite o número da agência");
+            Ag = Console.ReadLine();
+
+            Console.WriteLine("Digite o número da conta");
+            Conta = Console.ReadLine();
+        }
+
+         public void Op()
+        {
             Console.WriteLine("Selecione a opção desejada!");
             Console.WriteLine(" Opção 1 - Consulta saldo");
             Console.WriteLine(" Opção 2 - Saque");
             Console.WriteLine(" Opção 3 - Depósito");
+            Console.WriteLine(" Opção 4 - Sair");
 
             string selecao = Console.ReadLine();
 
             switch(selecao)
             {
                 case "1":
-                Console.WriteLine("Mostrar saldo");
+                Console.WriteLine($"Seu saldo é: {Saldo}");
                 break;
 
                 case "2":
@@ -31,10 +64,19 @@ namespace Banco.Models
                 Console.WriteLine("Realizar depósito");
                 break;
 
+                case "4":
+                Console.WriteLine("Obrigado!");
+                break;
+
                 default:
                 Console.WriteLine("Opção inválida");
                 break;
             }
+
+        }
+
+        public void MostrarSaldo()
+        {
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using Banco.Models;
 
-Operacoes op = new Operacoes();
+Operacoes login = new Operacoes();
+login.Login();
 
+
+Operacoes op = new Operacoes();
+// DadosConta dados = new DadosConta();
 op.Op();
+
 // DadosConta conta1 = new DadosConta(numeroConta: 125456, numeroAgencia: 1234);
 
 
